deaf57ce112653ac57fd781e75f8e5dc32cad69c
Grigory Efimov <grigory.efimov@gmail.com>
2023-11-30 19:25:08 +0300

    change common::consul_cfg to tools::consul_cfg

--- manifests/consensusnode/monitoring.pp
+++ manifests/consensusnode/monitoring.pp
@@ -1,6 +1,6 @@
 class anysync::consensusnode::monitoring {
   if $::anysync::monitoring {
-    common::consul_cfg { "any-sync-consensusnode": port => 8000 }
+    tools::consul_cfg { "any-sync-consensusnode": port => 8000 }
     collectd::cfg { "any-sync-consensusnode": content => inline_template("LoadPlugin processes\n<Plugin processes>\n    ProcessMatch \"any-sync-consensusnode\" \"/bin/any-sync-consensusnode\"\n</Plugin>\n") }
   }
 }
--- manifests/node/monitoring.pp
+++ manifests/node/monitoring.pp
@@ -1,6 +1,6 @@
 class anysync::node::monitoring {
   if $::anysync::monitoring {
-    common::consul_cfg { "any-sync-node": port => 8000 }
+    tools::consul_cfg { "any-sync-node": port => 8000 }
     collectd::cfg { "any-sync-node": content => inline_template("LoadPlugin processes\n<Plugin processes>\n    ProcessMatch \"any-sync-node\" \"/bin/any-sync-node\"\n</Plugin>\n") }
   }
 }
